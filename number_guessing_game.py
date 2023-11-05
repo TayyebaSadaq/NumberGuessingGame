8c73b9962506f30bd54b1625b0d8f7dbe06a861a
Tayyeba <official.tayyeba@gmail.com>
2023-11-05 17:12:52 +0000

    UPDATE FUNCTIONS

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -18,7 +18,18 @@ def menu():
         Infernal()
     else:
         print("Please enter a valid number from the menu") # NEED TO CATCH IF USER ENTERS STRING/INVALID INPUT
-    
+
+## REPLAY SELECTION ##   
+def play_again():
+    play = int(input("To return to the menu press 1, to exit press 2\n"))
+    if play == 1:
+        print("--------------------")
+        menu()
+    elif play == 2:
+        exit()
+    else:
+        print("Please enter a valid number from the menu")
+        
 ### DIFFICULTY SETTINGS ###
 def Easy():
     print("--------------------")
@@ -29,25 +40,6 @@ def Easy():
 ## GAME LOOP ##
     while counter < 20:
         guess = int(input("Please enter your guess: "))
-        if guess == number:
-            print(f"Congratulations! You guessed the number in {counter} attempts")
-            break
-        elif guess < number:
-            print("Your guess is too low")
-        elif guess > number:
-            print("Your guess is too high")
-        counter += 1
-    else:
-        print(f"You have run out of attempts, the number was {number}\n")
-## PLAY AGAIN ##
-    play = int(input("To return to the menu press 1, to exit press 2\n"))
-    if play == 1:
-        print("--------------------")
-        menu()
-    elif play == 2:
-        exit()
-    else:
-        print("Please enter a valid number from the menu") 
 
 def Medium():
     print("--------------------")
@@ -58,36 +50,34 @@ def Medium():
 ## GAME LOOP ##
     while counter < 15:
         guess = int(input("Please enter your guess: "))
-        if guess == number:
-            print(f"Congratulations! You guessed the number in {counter} attempts")
-            break
-        elif guess < number:
-            print("Your guess is too low")
-        elif guess > number:
-            print("Your guess is too high")
-        counter += 1
-    else:
-        print(f"You have run out of attempts, the number was {number}\n")
-## PLAY AGAIN ##
-    play = int(input("To return to the menu press 1, to exit press 2\n"))
-    if play == 1:
-        print("--------------------")
-        menu()
-    elif play == 2:
-        exit()
-    else:
-        print("Please enter a valid number from the menu")
+
 
 def Hard():
     print("--------------------")
+    print("""Hard mode selected.\nattempts = +10.\nThe number is between 1 and 50\nGood Luck!""")
+## SET VARIABLES ##
+    counter = 0
+    number = random.randint(1,50)
+## GAME LOOP ##
+    while counter < 10:
+        guess = int(input("Please enter your guess: "))
+
+
+
 
 def Infernal():
     print("--------------------")
-
+    print("""Infernal mode selected.\nattempts = +5.\nThe number is between 1 and 100\nGood Luck!""")
+## SET VARIABLES ##
+    counter = 0
+    number = random.randint(1,100)
+## GAME LOOP ##
+    while counter < 5:
+        guess = int(input("Please enter your guess: "))
+        
 
 # Main
-print("""Hello and welcome to the Number Guessing Game, as you can imagine the idea is that you have to
-try and guess a randomly generated number within a certain amount of attempts
-This game has 4 difficulty modes ~ Easy | Medium | Hard | Infernal.\n""")
+print("Hello and welcome to the Number Guessing Game.\nAs you can imagine the idea is thatnyou have to try and guess a randomly\ngenerated number within a certain amount of attempts\n")
+print("This game has 4 difficulty modes ~ \nEasy | Medium | Hard | Infernal.\n")
 menu()
     
