02e2f48662fb6e9ec191906c8af3fb8138896efd
Tayyeba <official.tayyeba@gmail.com>
2023-11-05 15:14:23 +0000

    start code

---
+++ number_guessing_game.py
@@ -0,0 +1,11 @@
+import random
+
+print("""Hello and welcome to the Number Guessing Game, as you can imagine the idea is that you have to
+try and guess a randomly generated number within a certain amount of attempts
+This game has 4 difficulty modes ~ Easy | Medium | Hard | Infernal.\n""")
+
+diff_choice = int(input("""Please enter a number from the menu to proceed:
+       1 - Easy
+       2 - Medium
+       3 - Hard
+       4 - Infernal \n"""))
